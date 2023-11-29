617e51084785cd69c3a12808f83b9a19b114f83e
Vic-Fabienne Wendt <vfschumann@gmail.com>
2023-11-29 11:17:05 +0100

    Update aufgabe2_4.ino

--- aufgabe2_4/aufgabe2_4.ino
+++ aufgabe2_4/aufgabe2_4.ino
@@ -12,7 +12,7 @@ void setup() {
   analogWrite(red, 255);          // warum analogWrite und nicht digital write? 
   analogWrite(green, 255);
   analogWrite(blue, 255);
-  Serial.begin(9600); // ??
+  Serial.begin(9600);             // by convention (s. Folien)
   Serial.print("Farbe");
   Serial.print("\t");
   Serial.println("Farbanteil");
@@ -21,28 +21,27 @@ void setup() {
 
 void loop() {
   
-  while (Serial.available()==0) {} // ??
+  while (Serial.available()==0) {}                       // ausgabe, wenn serial terminal geöffnet ist
 
-
-  cmd = Serial.readStringUntil(' ');
-  num = Serial.parseInt();
-  num = 255-constrain(num, 0, 255);
+    cmd = Serial.readStringUntil(' ');
+    num = Serial.parseInt();
+    num = 255-constrain(num, 0, 255);
+      
+    if(cmd == "off")  {                                // Wenn off eingegeben wird, geht die LED aus
+      analogWrite(red,255);
+      analogWrite(green,255);
+      analogWrite(blue,255);
+    }
     
-  if(cmd == "off")  {                                //Wenn off eingegeben wird, geht die LED aus
-    analogWrite(red,255);
-    analogWrite(green,255);
-    analogWrite(blue,255);
-  }
+    if(cmd == "red"){                                 // Wert für Rotanteil
+      analogWrite(red,num);
+    }
   
-  if(cmd == "red"){                                 //Wert für Rotanteil
-    analogWrite(red,num);
-  }
-
-  if(cmd == "green"){                               //Wert für Grünanteil
-    analogWrite(green,num);
-  }
-
-  if(cmd == "blue"){                                 //Wert für Blauanteil
-    analogWrite(blue,num);
+    if(cmd == "green"){                               // Wert für Grünanteil
+      analogWrite(green,num);
+    }
+  
+    if(cmd == "blue"){                                 // Wert für Blauanteil
+      analogWrite(blue,num);
   }    
 }
